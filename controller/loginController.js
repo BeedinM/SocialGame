2bde060af8108ad9716cf4cfd6b64ea8461630d0
BeedinM <douglas.bedin@gmail.com>
2022-05-13 16:25:03 -0300

    criação da comunicação do controlador com o modelo para o usuario fazer login

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,30 @@
+const bcryptjs = require('bcryptjs');
+
+const { Usuario } = require('../database/models')
+
 exports.fazerLogin = async ({email, senha}) => {
     
+    const usuario = await Usuario.findOne({
+        where: {
+            email
+        }
+    })
+    
+    if(!usuario) {
+        throw new Error('Acesso negado')
+    };
+
+    const { hashed } = usuario;
+
+    //comparar senha, ver se é válida
+    const isValid = bcryptjs.compareSync(senha, hashed);
+
+    //se não, throw error:
+    if(!isValid) {
+        throw new Error('Acesso negado')
+    };
+    
+    console.log(usuario.dataValues)
+
+    return usuario.dataValues;
 }
