b5ff7a2ac2a305d1c2600496f1d50b3824f1e9d2
BeedinM <douglas.bedin@gmail.com>
2022-05-13 15:12:36 -0300

    criação da rota post do login com método do controller

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const loginController = require('../controller/loginController')
 
 router.get('/', (req, res, next) => {
     res.render('login');
 });
+
+router.post('/', async (req, res, next) => {
+    const { email, senha } = req.body;
+
+    const { usuario_id: userId } = await loginController.fazerLogin({email, senha});
+
+    console.log(userId);
+})
  
 module.exports = router;
