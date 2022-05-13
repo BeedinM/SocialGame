73aac695a4155d9dfe7e217ae9715a5b72854de0
BeedinM <douglas.bedin@gmail.com>
2022-05-13 16:14:13 -0300

    correção do nome do banco de dados

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
       username: "root",
       password: "",
-      database: "smany",
+      database: "socialgame",
       host: "localhost",
       dialect: "mysql",
     },
