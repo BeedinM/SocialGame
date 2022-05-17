d28bd7a2057a2529569cca288f53ce2277e6efb2
BeedinM <douglas.bedin@gmail.com>
2022-05-17 14:50:33 -0300

    criação da rota post para criar conta

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const cadastroController = require('../controller/cadastroController');
+
 router.get('/', (req, res, next) => {
     res.render('cadastro');
+});
+
+router.post('/', async (req, res, next) => {
+    const { nomeUsuario, email, senha, reSenha } = req.body;
+
+    const usuario = await cadastroController.fazerCadastro({ nomeUsuario, email, senha, reSenha });
+    res.redirect('index');
 });
 
 module.exports = router;
