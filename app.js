335ee9f89943b5c2593d1cd7b1bf60d2571e14e4
BeedinM <douglas.bedin@gmail.com>
2022-05-17 14:31:56 -0300

    importação e criação da rota de cadastro no app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
+const cadastroRouter = require('./routes/cadastro');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/cadastro', cadastroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
